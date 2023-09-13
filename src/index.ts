999bf12426cf7a22a5689beeb5062c4326700e56
jawaka345 <lu.k@p2p.legal>
2023-09-13 19:17:00 +0200

    rename client2 into client

--- src/index.ts
+++ src/index.ts
@@ -54,10 +54,10 @@ io.sockets.on('connection', function (socket: Socket) {
 
     // Initialization
     console.log("connection from ", socket.id);
-    const client2 = new Client(socket, getRandomColor());
-    socket.emit('myId', socket.id, client2.label, client2.color, Date.now());
+    const client = new Client(socket, getRandomColor());
+    socket.emit('myId', socket.id, client.label, client.color, Date.now());
     socket.emit("server-version", PACKAGE.version);
-    socket.join(client2.board.roomId);
+    socket.join(client.board.roomId);
     
 
 
@@ -73,13 +73,13 @@ io.sockets.on('connection', function (socket: Socket) {
     socket.on("my_view", send_view_to_followers);
 
     function handle_update_self_user(label: string, color: string) {
-        client2.color = color;
-        client2.label = label;
-        client2.broadcast('update_user', socket.id, client2.label, client2.color, 0,0);
+        client.color = color;
+        client.label = label;
+        client.broadcast('update_user', socket.id, client.label, client.color, 0,0);
     }
 
     function handleGetRoomId() {
-        return client2.board.roomId;
+        return client.board.roomId;
     }
 
     function handleChangeRoomTo(roomIdAsked: string) {
@@ -88,29 +88,29 @@ io.sockets.on('connection', function (socket: Socket) {
             const board = boards.get(roomIdAsked);
             if (typeof board  !== "undefined"){
                 
-                client2.joinBoard(board);
+                client.joinBoard(board);
             }
         }
         else {
             console.log("asked room does not exist");
-            socket.emit("update_room_id", client2.board.roomId);
+            socket.emit("update_room_id", client.board.roomId);
         }
     }
 
     function handleDisconnect() {
         console.log("Handle: disconnect");
-        client2.broadcast('remove_user', socket.id);
+        client.broadcast('remove_user', socket.id);
     }
 
     function handleUpdateUser(x: number, y: number) {
         // console.log("Handle: update_user ", client.id, x, y)
-        client2.broadcast('update_user', socket.id, client2.label, client2.color, x, y);
+        client.broadcast('update_user', socket.id, client.label, client.color, x, y);
     }
 
     function add_follower(id: string) {
         console.log("ADDING FOLLOWER...");
-        if (client2.board.clients.has(id)) {
-            const user = client2.board.clients.get(id);
+        if (client.board.clients.has(id)) {
+            const user = client.board.clients.get(id);
             if (typeof user  != "undefined" ){
                 user.followers.push(socket.id);
                 io.to(id).emit("send_view");
@@ -121,8 +121,8 @@ io.sockets.on('connection', function (socket: Socket) {
 
     function remove_follower(id: string) {
         console.log("REMOVING FOLLOWER...");
-        if (client2.board.clients.has(id)) {
-            const user = client2.board.clients.get(id);
+        if (client.board.clients.has(id)) {
+            const user = client.board.clients.get(id);
             if (typeof user  != "undefined"){
                 const index = user.followers.indexOf(socket.id);
                 if (index > -1) {
@@ -136,7 +136,7 @@ io.sockets.on('connection', function (socket: Socket) {
 
     function send_view_to_followers(x: number, y: number, zoom: number) {
         // console.log("SEND VIEW TO FOLLOWERS:", x,y,zoom, client.id, users.get(client.id).followers);
-        const user = client2.board.clients.get(socket.id);
+        const user = client.board.clients.get(socket.id);
         if (user  !== undefined){
             for (const user_id of user.followers) {
                 io.to(user_id).emit("view_follower", x, y, zoom, socket.id);
@@ -152,26 +152,26 @@ io.sockets.on('connection', function (socket: Socket) {
 
     // Graph Actions
     // GraphPaste.addEvent(board, client);
-    socket.on("paste_graph", (verticesEntries: any[], linksEntries: any[]) => {GraphPaste.handle(client2.board, verticesEntries, linksEntries)});
+    socket.on("paste_graph", (verticesEntries: any[], linksEntries: any[]) => {GraphPaste.handle(client.board, verticesEntries, linksEntries)});
     // MergeVertices.addEvent(board, client);
-    socket.on("vertices_merge", (fixedVertexId: number, vertexToRemoveId: number) => MergeVertices.handle(client2.board, fixedVertexId, vertexToRemoveId));
+    socket.on("vertices_merge", (fixedVertexId: number, vertexToRemoveId: number) => MergeVertices.handle(client.board, fixedVertexId, vertexToRemoveId));
     // ApplyModifyer.addEvent(board, client);
-    socket.on("apply_modifyer", (name: string, attributesData: Array<any>) => ApplyModifyer.handle(client2.board, name, attributesData));
+    socket.on("apply_modifyer", (name: string, attributesData: Array<any>) => ApplyModifyer.handle(client.board, name, attributesData));
     // SubdivideLinkModification.addEvent(board, client);
-    socket.on("subdivide_link", (linkIndex: number, pos: {x: number, y: number}, callback: (response: number) => void) => {SubdivideLinkModification.handle(client2.board, linkIndex, new Coord(pos.x, pos.y), callback)} );
+    socket.on("subdivide_link", (linkIndex: number, pos: {x: number, y: number}, callback: (response: number) => void) => {SubdivideLinkModification.handle(client.board, linkIndex, new Coord(pos.x, pos.y), callback)} );
 
 
     // Board Generic
     // ResizeElement.addEvent(board, client);
-    socket.on("resize_element", ( kind: string, index: number, x: number, y: number, rawResizeType: string) => ResizeElement.handle(client2.board, kind, index, x, y, rawResizeType));
+    socket.on("resize_element", ( kind: string, index: number, x: number, y: number, rawResizeType: string) => ResizeElement.handle(client.board, kind, index, x, y, rawResizeType));
     // UpdateElement.addEvent(board, client);
-    socket.on("update_element", (kind: string, index: number, param: string, newValue: any) => UpdateElement.handle(client2.board, kind, index, param, newValue));
+    socket.on("update_element", (kind: string, index: number, param: string, newValue: any) => UpdateElement.handle(client.board, kind, index, param, newValue));
     // AddElement.addEvent(board, client);
-    socket.on("add_element", (kind: string, data: any, callback: (created_index: number) => void) => { AddElement.handle(client2.board, kind, data, callback)} );
+    socket.on("add_element", (kind: string, data: any, callback: (created_index: number) => void) => { AddElement.handle(client.board, kind, data, callback)} );
     // TranslateElements.addEvent(board, client);
-    socket.on("translate_elements", (indices: Array<[string, number]>, rawShift: {x: number, y: number}) => TranslateElements.handle(client2.board, indices, rawShift));
+    socket.on("translate_elements", (indices: Array<[string, number]>, rawShift: {x: number, y: number}) => TranslateElements.handle(client.board, indices, rawShift));
     // DeleteElements.addEvent(board, client);
-    socket.on("delete_elements", (indices: Array<[string, number]>) => { DeleteElements.handle(client2.board, indices)});
+    socket.on("delete_elements", (indices: Array<[string, number]>) => { DeleteElements.handle(client.board, indices)});
 
     // translate_elements // ne regarde pas écraser la dernière modif // TODO
     
@@ -186,21 +186,21 @@ io.sockets.on('connection', function (socket: Socket) {
 
     function handleUndo() {
         console.log("Handle: undo");
-        const modif = client2.board.cancel_last_modification();
+        const modif = client.board.cancel_last_modification();
         if (typeof modif === "string") {
             console.log(modif);
         } else {
-            modif.emitDeimplementation(client2.board);
+            modif.emitDeimplementation(client.board);
         }
     }
 
     function handleRedo() {
         console.log("Handle: redo");
-        const modif = client2.board.redo();
+        const modif = client.board.redo();
         if (typeof modif === "string") {
             console.log(modif);
         } else {
-            modif.emitImplementation(client2.board);
+            modif.emitImplementation(client.board);
         }
     }
 
@@ -208,27 +208,27 @@ io.sockets.on('connection', function (socket: Socket) {
 
     // JSON
     function handle_load_json(s: string) {
-        client2.board.graph.clear();
+        client.board.graph.clear();
 
         const data = JSON.parse(s);
         for (const vdata of data.vertices) {
             const newVertexData = new BasicVertexData(new Coord(data[1]["pos"]["x"], vdata[1]["pos"]["y"]), "", "black");
             const newVertex = new BasicVertex(vdata[0], newVertexData);
-            client2.board.graph.vertices.set(vdata[0], newVertex)
+            client.board.graph.vertices.set(vdata[0], newVertex)
         }
         for (const link of data.links) {
             // TODO
             //g.add_link_with_cp(link[1].start_vertex, link[1].end_vertex, link[1].orientation, new Coord(link[1].cp.x, link[1].cp.y))
         }
-        emit_graph_to_room(client2.board, new Set([SENSIBILITY.COLOR, SENSIBILITY.ELEMENT, SENSIBILITY.GEOMETRIC]));
+        emit_graph_to_room(client.board, new Set([SENSIBILITY.COLOR, SENSIBILITY.ELEMENT, SENSIBILITY.GEOMETRIC]));
     }
 
 
 
     function handle_get_json(callback: (arg0: string) => void) {
         const graph_stringifiable = {
-            vertices: Array.from(client2.board.graph.vertices.entries()),
-            links: Array.from(client2.board.graph.links.entries()),
+            vertices: Array.from(client.board.graph.vertices.entries()),
+            links: Array.from(client.board.graph.links.entries()),
         }
         callback(JSON.stringify(graph_stringifiable));
     }
